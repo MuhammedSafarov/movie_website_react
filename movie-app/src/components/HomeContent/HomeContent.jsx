3bed163e40b730b7aaa1b60d8c9effd346e494a9
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-03-09 15:50:22 +0400

    Optimized

--- movie-app/src/components/HomeContent/HomeContent.jsx
+++ movie-app/src/components/HomeContent/HomeContent.jsx
@@ -4,15 +4,15 @@ import "./HomeContent.scss";
 import MovieCards from "./MovieCards";
 
 const HomeContent = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
+  const [discoverMovies, setDiscoverMovies] = useState([]);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/tv/popular?api_key=d448aa11b683dfdce0641c3887f9a164&language=en-US&page=1"
+        "https://api.themoviedb.org/3/discover/movie?api_key=d448aa11b683dfdce0641c3887f9a164&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
       )
       .then((response) => {
-        setPopularMovies(response?.data?.results);
+        setDiscoverMovies(response?.data?.results);
         console.log(response);
       })
       .catch((err) => {
@@ -22,24 +22,13 @@ const HomeContent = () => {
 
   return (
     <div className="home-content">
-      <div className="content-header">
-        <div className="tick"></div>
-        <div className="content-name">New Movies</div>
+      <div className="discover-movies">
+        <div className="content-header">
+          <div className="tick"></div>
+          <div className="content-name">Discover Movies</div>
+        </div>
+        <MovieCards discoverMovies={discoverMovies} />
       </div>
-      <MovieCards popularMovies={popularMovies}/>
-
-      {/* <div className="movie-cards">
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-        <MovieCards/>
-      </div> */}
     </div>
   );
 };
