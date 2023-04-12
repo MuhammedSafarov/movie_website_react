f0c3316a900dd8294047b987cd2d4269f0ae25a7
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-04-12 21:35:40 +0400

    tvShowpage completed

--- movie-app/src/pages/DetailPage/TvShowsDetailPage.jsx
+++ movie-app/src/pages/DetailPage/TvShowsDetailPage.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import { getTvShowsAsync } from "../../store/actions/tvShows";
 
 const TvShowsDetailPage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const cardId = +useParams().id;
+  const [selectedShow, setSelectedShow] = useState([]);
+  const { allTvShows } = useSelector((state) => state.tvShows);
+  const dispatch = useDispatch();
 
-export default TvShowsDetailPage
+  const profile_img = (path) => {
+    return `https://www.themoviedb.org/t/p/w440_and_h660_face${path}`;
+  };
+
+  useEffect(() => {
+    dispatch(getTvShowsAsync());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setSelectedShow(allTvShows.find((item) => item.id === cardId));
+  }, [allTvShows, cardId]);
+
+  console.log(selectedShow);
+  return (
+    <div className="detail-container">
+      <div className="detail-card">
+        <img
+          className="detail-img"
+          src={profile_img(selectedShow?.backdrop_path)}
+          alt=""
+        />
+        <div className="detail-info">
+          <div className="movie-name">{selectedShow?.name}</div>
+          <div className="rating">
+            <b>Popularity: </b>
+            <svg
+              width="15"
+              height="15"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ipc-icon ipc-icon--star-inline star-svg"
+              id="iconContext-star-inline"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              role="presentation"
+            >
+              <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
+            </svg>
+            {selectedShow?.popularity?.toFixed(1)}
+          </div>
+          <div className="about-movie">{selectedShow?.overview}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TvShowsDetailPage;
